1c59d7ded092411ffe404ff9d26ad55f79e76f41
70_10 <7010.dev@gmail.com>
2025-01-01 11:44:53 +0900

    fix: Remove query parameters from URL in data sharing functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
           type="text"
           className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5"
           value={`${
-            window.location.href
+            window.location.href.split("?")[0]
           }?data=${lz.compressToEncodedURIComponent(
             JSON.stringify({ textA: text1, textB: text2 })
           )}`}
